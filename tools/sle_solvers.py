b4cd681a1638c71dd3720786ff5a3a06f36a547c
Ali Mehrabi <alimehrabi002@gmail.com>
2023-11-08 13:38:02 +0330

    Start Gauss-Seidel method for SLE solving

--- tools/sle_solvers.py
+++ tools/sle_solvers.py
@@ -5,7 +5,7 @@ This module provides several algorithms for solving systems of linear equations.
 import numpy as np
 
 
-def thomas(mat: np.ndarray, f: np.ndarray):
+def thomas(mat: np.ndarray, b: np.ndarray):
     """
     Solves tri-diagonal system of linear equeations using the Thomas algorithm.
 
@@ -29,7 +29,7 @@ def thomas(mat: np.ndarray, f: np.ndarray):
 
     # TODO: THIS BIT NEEDS OPTIMIZATION ==================
     a = np.array([0] + [mat[i, i - 1] for i in range(1, n)])
-    b = np.array([mat[i, i] for i in range(n)])
+    f = np.array([mat[i, i] for i in range(n)])
     c = np.array([mat[i, i + 1] for i in range(n - 1)] + [0])
     # ====================================================
     alpha = np.ndarray(shape=n)
@@ -37,14 +37,24 @@ def thomas(mat: np.ndarray, f: np.ndarray):
     y = np.ndarray(shape=n)
     x = np.ndarray(shape=n)
 
-    alpha[0], y[0] = b[0], f[0]
+    alpha[0], y[0] = f[0], b[0]
     for i in range(1, n):
         beta[i] = a[i] / alpha[i - 1]
-        alpha[i] = b[i] - beta[i] * c[i - 1]
-        y[i] = f[i] - beta[i] * y[i - 1]
+        alpha[i] = f[i] - beta[i] * c[i - 1]
+        y[i] = b[i] - beta[i] * y[i - 1]
 
     x[n - 1] = y[n - 1] / alpha[n - 1]
     for i in range(n - 2, -1, -1):
         x[i] = (y[i] - c[i] * x[i + 1]) / alpha[i]
 
     return x
+
+
+def gauss_seidel(
+    mat: np.ndarray,
+    b: np.ndarray,
+    x_0: np.ndarray = None,
+    eps: float = 1e-10,
+    stop_cond=lambda a, b, x: np.linalg.norm(b - np.matmul(a, x)) / np.norm(b),
+):
+    x = np.zeros_like(b) if x_0 is None else x_0
