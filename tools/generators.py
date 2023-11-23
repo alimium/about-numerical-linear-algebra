38f0485ce1657a27b19778e6085429c403f854c3
Ali Mehrabi <alimehrabi002@gmail.com>
2023-11-23 19:05:33 +0330

    Add Hilbert matrix generator

---
+++ tools/generators.py
@@ -0,0 +1,29 @@
+"""
+This module holds functions that generate different NLA stuff such as matrices and vectors
+"""
+
+import numpy as np
+
+
+def hilbert(n: int):
+    """
+    generate Hilbert matrices of size n efficiently.
+
+    Parameters
+    ----------
+        n(int): dimention of the matrix
+
+    Returns
+    -------
+        h(numpy.ndarray): generated Hilbert matrix
+    """
+
+    def _element_calc(i, j):
+        return np.ones_like(i) / (i + j - np.ones_like(i))
+
+    calc_func = np.vectorize(_element_calc)
+
+    i_s = np.array([(i + 1) * np.ones(shape=(n)) for i in range(n)])
+    j_s = np.array([(j + 1) * np.ones(shape=(n)) for j in range(n)]).T
+    h = calc_func(i_s, j_s)
+    return np.array(h)
