87479167fc8a06e373f6b51c18f1104af89b9042
Ali Mehrabi <alimehrabi002@gmail.com>
2023-11-23 19:34:24 +0330

    Fix docstring for hilbert

--- tools/generators.py
+++ tools/generators.py
@@ -1,13 +1,12 @@
 """
 This module holds functions that generate different NLA stuff such as matrices and vectors
 """
-
 import numpy as np
 
 
 def hilbert(n: int):
     """
-    generate Hilbert matrices of size n efficiently.
+    generate Hilbert matrices of size n.
 
     Parameters
     ----------
@@ -16,6 +15,10 @@ def hilbert(n: int):
     Returns
     -------
         h(numpy.ndarray): generated Hilbert matrix
+
+    Notes
+    -----
+        This currently returns Hilbert matrices of maximum size 10000 in a reasonable time.
     """
 
     def _element_calc(i, j):
@@ -26,4 +29,5 @@ def hilbert(n: int):
     i_s = np.array([(i + 1) * np.ones(shape=(n)) for i in range(n)])
     j_s = np.array([(j + 1) * np.ones(shape=(n)) for j in range(n)]).T
     h = calc_func(i_s, j_s)
-    return np.array(h)
+
+    return h
