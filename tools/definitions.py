bceba0635aa8ed8d336b50a1c6cd0b43307f4147
Ali Mehrabi <alimehrabi002@gmail.com>
2023-12-14 23:33:15 +0330

    Add eigenvalue calculation via scaled power method

--- tools/definitions.py
+++ tools/definitions.py
@@ -32,3 +32,45 @@ def condition_number(mat: np.ndarray, norm=2):
     n_mat_inv = np.linalg.norm(np.linalg.inv(mat), ord=norm)
 
     return n_mat * n_mat_inv
+
+
+def eigenvalue_scaled_power(
+    A: np.ndarray, v_0: np.ndarray, eps: float = 5e-5, max_iter: int = 100
+):
+    def stop_condition(l_k, l_k_1, eps):
+        return np.abs(l_k - l_k_1) <= eps
+
+    l = 0
+    iter_ = 1
+    v_prev = v_0
+    while max_iter > iter_:
+        v = A @ v_prev
+        new_l = np.linalg.norm(v, 1) / np.linalg.norm(v_prev, 1)
+        if stop_condition(new_l, l, eps):
+            return new_l
+        l = new_l
+        v_prev = v
+        iter_ += 1
+    print("reached max iter")
+    return l
+
+
+def eigenvalue_inverse_scaled_power(
+    A: np.ndarray, v_0: np.ndarray, eps: float = 5e-5, max_iter: int = 100
+):
+    def stop_condition(l_k, l_k_1, eps):
+        return np.abs(l_k - l_k_1) <= eps
+
+    l = 0
+    iter_ = 1
+    v_prev = v_0
+    while max_iter > iter_:
+        v = np.linalg.solve(A, v_0)
+        new_l = np.linalg.norm(v, 1) / np.linalg.norm(v_prev, 1)
+        if stop_condition(new_l, l, eps):
+            return new_l
+        l = new_l
+        v_prev = v
+        iter_ += 1
+    print("reached max iter")
+    return l
